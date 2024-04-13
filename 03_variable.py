6c87b26293398bdf8c021c9b09523d92881bb4dd
Darkness <agrawaldeepesh546@gmail.com>
2024-04-13 23:37:13 +0530

    03 python

---
+++ 03_variable.py
@@ -0,0 +1,23 @@
+
+# casting a varible
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print(x,y,z)
+
+# get the type of varibale 
+
+print("Type of x is :",type(x))
+
+
+# string can be declare in both single and double quote
+
+str1 = "darkness"
+str2 = 'darkness1'
+
+
+# variable name is case sensitive
+# camel case , pascal case , snake case
+
