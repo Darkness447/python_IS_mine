8e18f9cf0734582e942287f4e7f5540bbe167a28
Darkness <agrawaldeepesh546@gmail.com>
2024-04-28 12:30:19 +0530

    function

---
+++ 13_loops.py
@@ -0,0 +1,18 @@
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string). 
+
+# for x in array:
+
+# we can also loop through string
+# range function 
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+# Note that range(6) is not the values of 0 to 6, but the values 0 to 5.
+
+# range(2, 30, 3): the third parameter here is the till the value we want to increment 
+
+# for x in range(6):
+#   print(x)
+# else:
+#   print("Finally finished!") 
+
+# else statement here is used to execute when the loop is finised 
+# Note: The else block will NOT be executed if the loop is stopped by a break statement.
---
+++ 14_functions.py
@@ -0,0 +1,18 @@
+def my_func():
+    print("yes this is function")
+
+def arguHau(fname):
+    print(fname +" "+ "agrawal")
+    
+arguHau("deepesh")
+
+def funct(*arg): 
+    print("abdkjsbda")
+    # all the argument will be there
+# *arg receive tuple of argument **arg1 receive dictionary of argument'
+
+#  def my_function(country = "Norway"): same as any other language javascript
+#  pass , return ,
+
+# Any argument before the / , are positional-only, and any argument after the *, are keyword-only.
+
