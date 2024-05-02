ae4ca866c59f7cdbe44441425f8e9a4186f95566
Darkness <agrawaldeepesh546@gmail.com>
2024-05-02 23:08:28 +0530

    classes and object

---
+++ 16_array.py
@@ -0,0 +1,1 @@
+# list and array can be assume same but we can use list
---
+++ 17_class.py
@@ -0,0 +1,25 @@
+# python is object oriented programming language 
+# almost everything in python is an object
+# blueprint
+
+
+class HowToMakeAClass:
+    def __init__():
+        print("kuch nhi system hai")
+    x=5
+    
+# self is refrence to current object of the class then 
+# jai shree  ram
+
+# self is the first parameter of any function very important
+# constructor in class is like __init__()
+
+# how to delete the property from the object very important
+# del p1.age 
+
+
+# if your class object is empty then put that into
+
+# create an object
+p1=HowToMakeAClass()
+print(p1.x)
