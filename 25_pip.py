62ec1efa351b88af224c1384fe500c6dd1aead09
Darkness <agrawaldeepesh546@gmail.com>
2024-05-03 21:36:22 +0530

    pip

--- 25_pip.py
+++ 25_pip.py
@@ -2,3 +2,15 @@
 
 # If you do not have PIP installed, you can download and install it from this page: https://pypi.org/project/pip/ 
 
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
+  
+#   finally can be used to clean up the resources very important
