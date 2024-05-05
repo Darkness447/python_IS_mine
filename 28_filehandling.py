07b41f3c6e7c4d53aaff2c53924142cec8d4a1af
Darkness <agrawaldeepesh546@gmail.com>
2024-05-05 19:10:43 +0530

    file handling

---
+++ 28_filehandling.py
@@ -0,0 +1,18 @@
+# we can handle files in python
+# four different mode to open file are r,a,w,c 
+# read , append , write ,create 
+
+# b binary mode (images) t for text
+
+# # w will overwrite 
+# f=open("darkness.txt","a")
+# f.write("yes that is true")
+# f.close()
+
+# to delete a file must incorporate os module 
+
+# import os 
+# os.remove("demofile.txt")
+# check if file exist
+# os.rmdir() remove the whole folder
+
